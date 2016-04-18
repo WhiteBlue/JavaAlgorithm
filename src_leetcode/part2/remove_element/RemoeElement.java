ba9716af5e4702d93fff896b171a79f73c2cdfa5
WhiteBlue <whiteblue616@icloud.com>
2016-04-19 01:37:03 +0800

    daily

---
+++ src_leetcode/part2/remove_element/RemoeElement.java
@@ -0,0 +1,23 @@
+package part2.remove_element;
+
+/**
+ * id: 27
+ * <p>
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ */
+public class RemoeElement {
+
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
